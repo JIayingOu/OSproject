adb3a0c680e6a023db958646d730ec072123c400
Jiaying OU <530712644@qq.com>
2016-08-24 20:46:29 +0800

    add 3 files

--- OSOrderManagementSystem.php
+++ OSOrderManagementSystem.php
@@ -1,6 +1,66 @@
 <html>
 <head>
+<title>欧盛订单管理系统</title>
+<meta charset="utf-8" />
+<link href="../bootstrap-3.3.6/dist/css/bootstrap.css" rel="stylesheet" />
+<script src="../jquery/jquery-3.0.0.js"></script>
+<script src="../bootstrap-3.3.6/dist/js/bootstrap.js"></script>
+<style>
+.containar
+{
+	width: 100%;
+	height: 100%;
+}
+.logonUI
+{
+	position:absolute;
+	top:20%;
+	left:40%;
+	width:350px;
+	height:250px;
+	border:1px #000000 solid;
+}
+</style>
 </head>
+
 <body>
+<div class="containar">
+     <form class="logonUI" action="" method="post">        
+          <span>账号:</span>
+          <input type="text" id="inputAccount" name="adminName"><br />		  
+          <span>密码:</span>
+          <input type="password" id="inputPassword" name="password"><br />         
+          <button type="button" class="btn btn-success" id="logonButton">登录</button>
+     </form>
+</div>
+
+<script>
+$(document).ready(function(){
+    $("#logonButton").click(function()
+	{
+		systemlogon();
+	});
+});
+
+function systemlogon()
+{
+	if($("#inputAccount").val() == "" || $("#inputAccount").val() == null)
+	{
+		alert("账号不能为空！");
+	}
+	else if($("#inputPassword").val() == "" || $("#inputPassword").val() == null)
+	     {
+			 alert("密码不能为空！");
+		 }
+		 else formSubmitOfLogon();
+}
+
+function formSubmitOfLogon()
+{
+	form.action="logon.php";
+	form.submit();
+}
+</script>
 </body>
+
 </html>
